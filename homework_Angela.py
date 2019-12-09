8af03bc6f2ad6a814cc9564d5f7bba6d2007c278
AngelaAsatryan <58570091+AngelaAsatryan@users.noreply.github.com>
2019-12-09 13:31:38 +0400

    Add files via upload

---
+++ homework_Angela.py
@@ -0,0 +1,10 @@
+name = "Angela"
+surname = "Asatryan"
+print(name, surname)
+
+for x in range(0, len(name)):
+    print(name[x], end=" ")
+print("")
+for y in range(0, len(surname)):
+    print(surname[y], end=" ")
+
