fe4ff61e2782213ca6e8ddded8b0e6462e7f40bb
AngelaAsatryan <58570091+AngelaAsatryan@users.noreply.github.com>
2019-12-24 14:43:50 +0400

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -4,6 +4,23 @@
 myfile=open(r"Homework3.txt", "r")
 my_file=myfile.read()
 
+#define punctuation marks
+punc_marks=("," "." "?" ";" "-" ":" "!")
+#define a function to count each punctuation mark seperately
+def count_each_mark(punc_mark):
+        print("The number of"+ " " +str(punc_mark) + " " +"mark in the file is" + " " +str(my_file.count(punc_mark))+".")
+
+#print each mark
+for i in range(0, len(punc_marks)):
+    count_each_mark(str(punc_marks[i]))
+
+#calculate the sum of the punctuations marks used in the texte file
+def sum_punc_marks(punc_mark):
+        total_punc_marks=my_file.count(str(punc_mark[0]))+my_file.count(str(punc_mark[1]))+my_file.count(str(punc_mark[2]))+my_file.count(str(punc_mark[3]))+my_file.count(str(punc_mark[4]))+my_file.count(str(punc_mark[5]))+my_file.count(str(punc_mark[6]))
+        print("There are" + " " + str(total_punc_marks) + " " + "punctuation marks used in the text.")
+sum_punc_marks(str(punc_marks))
+
+##other way to calculate the sum of the punctuations marks used in the texte file
 #find if there any $ symbol in the text, if yes, replace and go to the next step
 find_symbol = my_file.find('$')
 input = ",.?;-:!"
@@ -22,21 +39,3 @@ else:
 punc_mark_count=(my_file_no_punc.count('$'))
 text_to_print= "There are"+ " " + str(punc_mark_count) + " " + "punctuation marks used in the text."
 print(text_to_print)
-
-#define a function to count each punctuation mark seperately
-def count_each_mark(punc_mark):
-        print("The number of"+ " " +str(punc_mark) + " " +"mark in the file is" + " " +str(my_file.count(punc_mark))+".")
-
-punc_marks=("," "." "?" ";" "-" ":" "!")
-#print each mark
-for i in range(0, len(punc_marks)):
-    count_each_mark(str(punc_marks[i]))
-
-
-
-###other way to calculate the sum of the punctuations marks used in the texte file
-# print(sum(my_file.count(str(punc_marks[0]));my_file.count(str(punc_marks[6]))))
-def sum_punc_marks(punc_mark):
-        total_punc_marks=my_file.count(str(punc_mark[0]))+my_file.count(str(punc_mark[1]))+my_file.count(str(punc_mark[2]))+my_file.count(str(punc_mark[3]))+my_file.count(str(punc_mark[4]))+my_file.count(str(punc_mark[5]))+my_file.count(str(punc_mark[6]))
-        print("There are" + " " + str(total_punc_marks) + " " + "punctuation marks used in the text.")
-sum_punc_marks(str(punc_marks))
